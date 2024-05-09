068f73a258f51b6400f5c21c3e254c0436026153
danaiass <168175264+danaiass@users.noreply.github.com>
2024-05-09 10:53:47 +0700

    ส่งการบ้าน QUIZ

---
+++ index.js
@@ -0,0 +1,28 @@
+let count = 0; 
+
+function checkAnswer1(answer) {
+    const result = document.getElementById('result1');
+    if (answer === 'a') {
+        result.textContent = "ถูกต้อง PIM ย่อมาจาก PIM";
+        result.style.color = 'green';
+        count++;
+    } else {
+        result.textContent = "Incorrect! Try again.";
+        result.style.color = 'red';
+    }
+}
+
+function checkAnswer2(answer) {
+    const result = document.getElementById('result2');
+    const result3 = document.getElementById('result3');
+    if (answer === 'b') {
+        result.textContent = "ถูกต้อง HRSS อยู่สายงาน HR";
+        result.style.color = 'green';
+        count++;
+    } else {
+        result.textContent = "Incorrect! Try again.";
+        result.style.color = 'red';
+    }
+    result3.textContent = "คุณได้ทั้งหมด " + count + " คะแนน";
+    count = 0;
+}
